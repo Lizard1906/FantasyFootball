3cd81014ac1ca42321c041f142438368a748b1c5
Lizard1906 <canaleatorio2021@gmail.com>
2024-07-04 12:08:38 +0100

    fix points

--- data/js/data.js
+++ data/js/data.js
@@ -72,16 +72,16 @@ const trophies = [
     {
         id: 'euro24', code: 'euro24', date: 2024, name: 'Euro Cup', category: 'euro',
         graph: [
-            { player: 1, color: 'red', evolution: [56, 90, 46, 68] },
-            { player: 2, color: 'green', evolution: [56, 58, 34, 74] },
+            { player: 1, color: 'red', evolution: [56, 146, 192, 260] },
+            { player: 2, color: 'green', evolution: [56, 114, 148, 222] },
         ],
         finished: false
     },
     {
         id: 'euro24predictor', code: 'euro24predictor', date: 2024, name: 'Euro Cup Predict', category: 'euro',
         graph: [
-            { player: 1, color: 'red', evolution: [69, 81, 57, 47] },
-            { player: 2, color: 'green', evolution: [74, 106, 70, 60] },
+            { player: 1, color: 'red', evolution: [69, 150, 207, 254] },
+            { player: 2, color: 'green', evolution: [74, 180, 250, 310] },
         ],
         finished: false
     },
